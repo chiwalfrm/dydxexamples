7028ffbdf8679b45b560ccae2376e4789bc83e8a
chiwalfrm <chiuljnk@hotmail.com>
2022-04-11 13:41:27 -0500

    Update dydxtrades.py

--- dydxtrades.py
+++ dydxtrades.py
@@ -1,12 +1,33 @@
 import datetime
 import json
+import logging
+import os
 import pprint
 import sys
+import time
+from logging.handlers import RotatingFileHandler
+from os.path import exists
 from websocket import create_connection
 
+ramdiskpath = '/mnt/ramdisk'
+logger = logging.getLogger("Rotating Log")
+logger.setLevel(logging.INFO)
+
 pp = pprint.PrettyPrinter(width = 41, compact = True)
+if len(sys.argv) < 2:
+        market = 'BTC-USD'
+else:
+        market = sys.argv[1]
+handler = RotatingFileHandler(ramdiskpath+'/dydxtrades'+market+'.log', maxBytes=1048576,
+                              backupCount=4)
+logger.addHandler(handler)
+if exists(ramdiskpath) == False:
+        print('Error: Ramdisk', ramdiskpath, 'not mounted')
+        exit()
+if os.path.ismount(ramdiskpath) == False:
+        print('Warning:', ramdiskpath, 'is not a mount point')
 ws = create_connection("wss://api.dydx.exchange/v3/ws")
-api_data = {"type":"subscribe", "channel":"v3_trades", "id":"BTC-USD"}
+api_data = {"type":"subscribe", "channel":"v3_trades", "id":market}
 ws.send(json.dumps(api_data))
 api_data = ws.recv()
 api_data = json.loads(api_data)
@@ -16,21 +37,21 @@ api_data = json.loads(api_data)
 pp.pprint(api_data)
 while True:
         try:
-                api_data = ws.recv()
-                api_data = json.loads(api_data)
                 trades = api_data['contents']['trades'][0]
                 tradecreatedat = trades['createdAt']
                 tradeprice = trades['price']
                 tradeside = trades['side']
                 tradesize = trades['size']
-                with open('/mnt/ramdisk/lasttrade', "w") as fp:
+                with open(ramdiskpath+'/'+market+'/lasttrade', "w") as fp:
                         fp.write(tradecreatedat+' '+tradeprice+' '+tradeside+' ('+tradesize+')\n')
                 fp.close()
-                print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), tradecreatedat, tradeprice, tradeside.ljust(4), '('+tradesize+')')
+                logger.info(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' '+tradecreatedat+' '+tradeprice+' '+tradeside.ljust(4)+' ('+tradesize+')')
+                api_data = ws.recv()
+                api_data = json.loads(api_data)
         except KeyboardInterrupt:
                 ws.close()
                 sys.exit(0)
         except Exception as error:
-                print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" WebSocket message failed (%s)" % error)
+                print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "WebSocket message failed (%s)" % error)
                 ws.close()
                 time.sleep(1)
