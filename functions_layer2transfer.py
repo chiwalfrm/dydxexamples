139e336c6c8548672397f08bcb69ecc916115437
chiwalfrm <chiuljnk@hotmail.com>
2022-06-03 11:43:09 -0500

    Create functions_layer2transfer.py

---
+++ functions_layer2transfer.py
@@ -0,0 +1,10 @@
+#transferl2amount/receiverid/receiverkey/receiverpositionid (SET THESE)
+def transferl2():
+        create_transfer_result = client.private.create_transfer(
+                amount = transferl2amount,
+                position_id = account['positionId'],
+                expiration = epoch_seconds_to_iso(time.time() + 604801),
+                receiver_account_id = receiverid,
+                receiver_public_key = receiverkey,
+                receiver_position_id = receiverpositionid,
+        )
