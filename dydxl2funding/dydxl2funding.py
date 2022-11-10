502812e9901fd5fe1f3d53449c64310628f28a07
chiwalfrm <chiuljnk@hotmail.com>
2022-11-10 03:57:45 -0600

    Create dydxl2funding.py

---
+++ dydxl2funding/dydxl2funding.py
@@ -0,0 +1,19 @@
+from dydx3 import Client
+from dydx3 import constants
+import pprint
+import sys
+
+pp = pprint.PrettyPrinter(width = 41, compact = True)
+
+_network_id = str(constants.NETWORK_ID_MAINNET)
+#_network_id is set to either str(constants.NETWORK_ID_MAINNET) or str(constants.NETWORK_ID_GOERLI)
+_api_host = constants.API_HOST_MAINNET
+#_api_host is set to either constants.API_HOST_MAINNET or constants.API_HOST_GOERLI
+
+client = Client(
+        host = _api_host,
+        network_id = _network_id
+)
+
+historical_funding = client.public.get_historical_funding(market = sys.argv[1])
+pp.pprint(historical_funding.data)
