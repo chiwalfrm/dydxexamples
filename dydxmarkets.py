a75d15f5baa00fc656fd91b103db4b3b734c543c
chiwalfrm <chiuljnk@hotmail.com>
2023-01-27 15:54:42 -0600

    Update dydxmarkets.py

--- dydxmarkets.py
+++ dydxmarkets.py
@@ -1,5 +1,9 @@
 import requests
 #r = requests.get(url = 'https://api.stage.dydx.exchange/v3/markets')
 r = requests.get(url = 'https://api.dydx.exchange/v3/markets')
-for key, value in r.json()['markets'].items():
-        print(key, value['status'])
+r.raise_for_status()
+if r.status_code == 200:
+        for key, value in r.json()['markets'].items():
+                print(key, value['status'])
+else:
+        print('Bad requests status code:', r.status_code)
