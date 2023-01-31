139bb3ef87937837c104fb917f80fcd7458a622c
chiwalfrm <chiuljnk@hotmail.com>
2023-01-31 14:10:10 -0600

    Update demodydxcreateorder_encrypted.py

--- demodydxcreateorder_encrypted.py
+++ demodydxcreateorder_encrypted.py
@@ -2,22 +2,24 @@ from cryptography.fernet import Fernet
 from dydx3 import Client
 from dydx3 import constants
 from dydx3 import epoch_seconds_to_iso
+from os import path
 import base64
+import datetime
+import requests
+import sys
 import time
 
 ########################## YOU FILL THIS OUT #################
-_private_key_encrypted = '<FILL_THIS_OUT>'
-#_private_key_encrypted is optional and may be set to '' (hardware wallets do not generally provide this information)
-#If _private_key_encrypted is set, you do not need to set _api_key_encrypted/_api_secret_encrypted/_api_passphrase_encrypted/_stark_private_key_encrypted
-_api_key_encrypted = '<FILL_THIS_OUT>'
-_api_secret_encrypted = '<FILL_THIS_OUT>'
-_api_passphrase_encrypted = '<FILL_THIS_OUT>'
-_stark_private_key_encrypted = '<FILL_THIS_OUT>'
-_eth_address_encrypted = '<FILL_THIS_OUT>'
-_network_id = str(constants.NETWORK_ID_GOERLI)
-#_network_id is set to either str(constants.NETWORK_ID_MAINNET) or str(constants.NETWORK_ID_GOERLI)
-_api_host = constants.API_HOST_GOERLI
-#_api_host is set to either constants.API_HOST_MAINNET or constants.API_HOST_GOERLI
+my_eth_private_key_encrypted = '<FILL_THIS_OUT>'
+#my_eth_private_key_encrypted is optional and may be set to '' (hardware wallets do not generally provide this information)
+#If my_eth_private_key_encrypted is set, you do not need to set my_api_key_encrypted/my_api_secret_encrypted/my_api_passphrase_encrypted/my_stark_private_key_encrypted
+my_api_key_encrypted = '<FILL_THIS_OUT>'
+my_api_secret_encrypted = '<FILL_THIS_OUT>'
+my_api_passphrase_encrypted = '<FILL_THIS_OUT>'
+my_stark_private_key_encrypted = '<FILL_THIS_OUT>'
+my_eth_address_encrypted = '<FILL_THIS_OUT>'
+my_network_id = str(constants.NETWORK_ID_GOERLI)
+#my_network_id is set to either str(constants.NETWORK_ID_MAINNET) or str(constants.NETWORK_ID_GOERLI)
 ##############################################################
 
 def encrypt(message: bytes, key: bytes) -> bytes:
@@ -25,6 +27,18 @@ def encrypt(message: bytes, key: bytes) -> bytes:
 
 def decrypt(token: bytes, key: bytes) -> bytes:
         return Fernet(key).decrypt(token)
+
+if path.exists(sys.argv[1]):
+        exec(open(sys.argv[1]).read())
+if my_api_network_id == str(constants.NETWORK_ID_MAINNET):
+        my_api_host = constants.API_HOST_MAINNET
+        my_ws_host = constants.WS_HOST_MAINNET
+elif my_api_network_id == str(constants.NETWORK_ID_GOERLI):
+        my_api_host = constants.API_HOST_GOERLI
+        my_ws_host = constants.WS_HOST_GOERLI
+else:
+        print('Error: my_api_network_id is not '+str(constants.NETWORK_ID_MAINNET)+' or '+str(constants.NETWORK_ID_GOERLI)+'.')
+        exit()
 
 print("Enter decryption key (type 'encrypt' to encrypt): ", end = '')
 decryptionkey = input()
@@ -43,37 +57,37 @@ if decryptionkey == 'encrypt':
                         exit()
                 encryptedmessage = encrypt(encryptstring.encode(), encryptkey)
                 print(base64.b64encode(encryptedmessage))
-_eth_address = decrypt(base64.b64decode(_eth_address_encrypted), decryptionkey).decode()
+my_eth_address = decrypt(base64.b64decode(my_eth_address_encrypted), decryptionkey).decode()
 
-if _private_key != '':
-        _private_key = decrypt(base64.b64decode(_private_key_encrypted), decryptionkey).decode()
+if my_eth_private_key_encrypted != '':
+        my_eth_private_key = decrypt(base64.b64decode(my_eth_private_key_encrypted), decryptionkey).decode()
         client = Client(
-                host = _api_host,
-                default_ethereum_address = _eth_address,
-                eth_private_key = _private_key,
-                network_id = _network_id
+                host = my_api_host,
+                default_ethereum_address = my_eth_address,
+                eth_private_key = my_eth_private_key,
+                network_id = my_network_id
         )
         derive_stark_key_result = client.onboarding.derive_stark_key()
         stark_private_key = derive_stark_key_result['private_key']
         client.stark_private_key = stark_private_key
 else:
-        _api_key = decrypt(base64.b64decode(_api_key_encrypted), decryptionkey).decode()
-        _api_secret = decrypt(base64.b64decode(_api_secret_encrypted), decryptionkey).decode()
-        _api_passphrase = decrypt(base64.b64decode(_api_passphrase_encrypted), decryptionkey).decode()
-        _stark_private_key = decrypt(base64.b64decode(_stark_private_key_encrypted), decryptionkey).decode()
+        my_api_key = decrypt(base64.b64decode(my_api_key_encrypted), decryptionkey).decode()
+        my_api_secret = decrypt(base64.b64decode(my_api_secret_encrypted), decryptionkey).decode()
+        my_api_passphrase = decrypt(base64.b64decode(my_api_passphrase_encrypted), decryptionkey).decode()
+        my_stark_private_key = decrypt(base64.b64decode(my_stark_private_key_encrypted), decryptionkey).decode()
         client = Client(
-                host = _api_host,
-                network_id = _network_id,
+                host = my_api_host,
+                network_id = my_network_id,
                 api_key_credentials = {
-                        'key': _api_key,
-                        'secret': _api_secret,
-                        'passphrase': _api_passphrase
+                        'key': my_api_key,
+                        'secret': my_api_secret,
+                        'passphrase': my_api_passphrase
                 }
         )
-        client.stark_private_key = _stark_private_key
+        client.stark_private_key = my_stark_private_key
 
 get_account_result = client.private.get_account(
-        ethereum_address = _eth_address
+        ethereum_address = my_eth_address
 )
 account = get_account_result.data['account']
 one_minute_from_now_iso = epoch_seconds_to_iso(time.time() + 70)
