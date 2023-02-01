6ade53a12c1cbcf7086b93a32ac061624c8e3242
chiwalfrm <chiuljnk@hotmail.com>
2023-02-01 13:13:26 -0600

    Update dydxmarkets.py

--- dydxmarkets.py
+++ dydxmarkets.py
@@ -1,6 +1,6 @@
-import requests
-#r = requests.get(url = 'https://api.stage.dydx.exchange/v3/markets')
-r = requests.get(url = 'https://api.dydx.exchange/v3/markets')
+from requests import get
+#r = get(url = 'https://api.stage.dydx.exchange/v3/markets')
+r = get(url = 'https://api.dydx.exchange/v3/markets')
 r.raise_for_status()
 if r.status_code == 200:
         for key, value in r.json()['markets'].items():
