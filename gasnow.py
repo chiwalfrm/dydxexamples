f830b31fb062036e740e0e4c41314e260065e7d5
chiwalfrm <chiuljnk@hotmail.com>
2023-02-01 13:12:14 -0600

    Update gasnow.py

--- gasnow.py
+++ gasnow.py
@@ -1,5 +1,5 @@
-import requests
-r = requests.get(url = 'https://etherchain.org/api/gasnow')
+from requests import get
+r = get(url = 'https://etherchain.org/api/gasnow')
 for key, value in r.json()['data'].items():
         if key != 'timestamp' and key != 'priceUSD':
                 print(key.ljust(8), str(int(value/1000000000)).rjust(3))
