548b004883dfdc325e1c77d72a819cafc505557c
chiwalfrm <chiuljnk@hotmail.com>
2023-02-01 13:16:54 -0600

    Update demodydxhistory.py

--- demodydxhistory.py
+++ demodydxhistory.py
@@ -1,9 +1,9 @@
+import datetime
+from ciso8601 import parse_datetime
 from dydx3 import Client
 from dydx3 import constants
 from os import path
-import ciso8601
-import datetime
-import sys
+from sys import argv
 
 ########################## YOU FILL THIS OUT #################
 _private_key = '<FILL_THIS_OUT>'
@@ -20,12 +20,12 @@ _api_host = constants.API_HOST_GOERLI
 #_api_host is set to either constants.API_HOST_MAINNET or constants.API_HOST_GOERLI
 ##############################################################
 
-if len(sys.argv) > 3:
-        if not path.exists(sys.argv[3]):
-                print('ERROR: File', sys.argv[3], 'does not exist.')
+if len(argv) > 3:
+        if not path.exists(argv[3]):
+                print('ERROR: File', argv[3], 'does not exist.')
                 exit()
         else:
-                exec(open(sys.argv[3]).read())
+                exec(open(argv[3]).read())
                 _private_key = my_eth_private_key
                 _api_key = my_api_key
                 _api_secret = my_api_secret
@@ -62,18 +62,18 @@ else:
                 }
         )
 
-if len(sys.argv) > 1:
-        start_timestamp = sys.argv[1]
+if len(argv) > 1:
+        start_timestamp = argv[1]
 else:
         start_timestamp = datetime.datetime.utcnow() + datetime.timedelta(days = -7)
         start_timestamp = start_timestamp.isoformat()[:-3] + 'Z'
-if len(sys.argv) > 2:
-        stop_timestamp = sys.argv[2]
+if len(argv) > 2:
+        stop_timestamp = argv[2]
 else:
         stop_timestamp = datetime.datetime.utcnow()
         stop_timestamp = stop_timestamp.isoformat()[:-3] + 'Z'
 list_of_fills = [ ]
-while ciso8601.parse_datetime(stop_timestamp) > ciso8601.parse_datetime(start_timestamp):
+while parse_datetime(stop_timestamp) > parse_datetime(start_timestamp):
         get_fills_results = client.private.get_fills(created_before_or_at = stop_timestamp)
         get_fills_results = get_fills_results.data
         firstrecorddate = get_fills_results['fills'][0]['createdAt']
@@ -83,11 +83,11 @@ while ciso8601.parse_datetime(stop_timestamp) > ciso8601.parse_datetime(start_ti
                 print('Please contact customer service for further assistance.')
                 exit()
         for fill in get_fills_results['fills']:
-                if ciso8601.parse_datetime(fill['createdAt']) > ciso8601.parse_datetime(start_timestamp):
+                if parse_datetime(fill['createdAt']) > parse_datetime(start_timestamp):
                         list_of_fills.append(fill)
         if len(get_fills_results['fills']) < 100:
                 break
-        stop_timestamp = ciso8601.parse_datetime(get_fills_results['fills'][-1]['createdAt']) + datetime.timedelta(microseconds = 1000)
+        stop_timestamp = parse_datetime(get_fills_results['fills'][-1]['createdAt']) + datetime.timedelta(microseconds = 1000)
         stop_timestamp = stop_timestamp.isoformat()[:-9] + 'Z'
 list_of_fills_unique = [ ]
 for fill in list_of_fills:
