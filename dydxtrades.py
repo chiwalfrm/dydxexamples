55086992ce58a30d517e2b822f84056ddf2a7ce4
chiwalfrm <chiuljnk@hotmail.com>
2023-02-01 14:00:43 -0600

    Update dydxtrades.py

--- dydxtrades.py
+++ dydxtrades.py
@@ -56,8 +56,10 @@ if len(sys.argv) < 2:
         market = 'BTC-USD'
 else:
         market = sys.argv[1]
-handler = logging.handlers.RotatingFileHandler(ramdiskpath+'/dydxtrades'+market+'.log', maxBytes=2097152,
-                              backupCount = 4)
+handler = logging.handlers.RotatingFileHandler(ramdiskpath+'/dydxtrades'+market+'.log',
+        maxBytes=2097152,
+        backupCount = 4
+)
 logger.addHandler(handler)
 
 if os.path.exists(ramdiskpath) == False:
