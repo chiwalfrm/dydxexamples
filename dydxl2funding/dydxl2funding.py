5772f00f0073ce28ef0da2c562cdb88f2b08d3c1
chiwalfrm <chiuljnk@hotmail.com>
2023-02-06 01:47:07 -0600

    Update dydxl2funding.py

--- dydxl2funding/dydxl2funding.py
+++ dydxl2funding/dydxl2funding.py
@@ -1,19 +1,19 @@
-import pprint
-import sys
 from dydx3 import Client
 from dydx3 import constants
+from pprint import PrettyPrinter
+from sys import argv
 
-pp = pprint.PrettyPrinter(width = 41, compact = True)
+pp = PrettyPrinter(width = 41, compact = True)
 
-_network_id = str(constants.NETWORK_ID_MAINNET)
-#_network_id is set to either str(constants.NETWORK_ID_MAINNET) or str(constants.NETWORK_ID_GOERLI)
-_api_host = constants.API_HOST_MAINNET
-#_api_host is set to either constants.API_HOST_MAINNET or constants.API_HOST_GOERLI
+my_api_network_id = str(constants.NETWORK_ID_MAINNET)
+#my_api_network_id is set to either str(constants.NETWORK_ID_MAINNET) or str(constants.NETWORK_ID_GOERLI)
+my_api_host = constants.API_HOST_MAINNET
+#my_api_host is set to either constants.API_HOST_MAINNET or constants.API_HOST_GOERLI
 
 client = Client(
-        host = _api_host,
-        network_id = _network_id
+        host = my_api_host,
+        network_id = my_api_network_id
 )
 
-historical_funding = client.public.get_historical_funding(market = sys.argv[1])
+historical_funding = client.public.get_historical_funding(market = argv[1])
 pp.pprint(historical_funding.data)
