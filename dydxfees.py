9d06f272b064c701903859430a9856a9e6f2a686
chiwalfrm <chiuljnk@hotmail.com>
2023-02-25 23:36:21 -0600

    Update dydxfees.py

--- dydxfees.py
+++ dydxfees.py
@@ -1,5 +1,6 @@
 from dydx3 import Client
 from dydx3 import constants
+from datetime import datetime
 from requests import get
 
 client = Client(
@@ -7,20 +8,21 @@ client = Client(
         network_id = constants.NETWORK_ID_MAINNET
 )
 
+timestamp=datetime.now()
 #r = get(url = 'https://api.stage.dydx.exchange/v3/markets')
 r = get(url = 'https://api.dydx.exchange/v3/markets')
 r.raise_for_status()
 if r.status_code == 200:
         totalfees = 0
-        for key, value in r.json()['markets'].items():
+        for key, value in sorted(r.json()['markets'].items()):
                 market_statistics1 = client.public.get_stats(
                         market = key,
                         days = constants.MARKET_STATISTIC_DAY_ONE,
                 )
                 fees = market_statistics1.data['markets'][key]['fees']
                 totalfees = totalfees + float(fees)
-                print(key, fees)
+                print(timestamp, key.ljust(10), fees.rjust(14))
 #               print(market_statistics.headers)
-        print('TOTAL', totalfees)
+        print(timestamp, 'TOTAL     ', '{0:.6f}'.format(totalfees).rjust(14))
 else:
         print('Bad requests status code:', r.status_code)
