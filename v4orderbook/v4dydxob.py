05a934544fe889e609390b3f416e90e35e5d62fa
chiwalfrm <chiuljnk@hotmail.com>
2023-09-05 08:28:15 -0500

    Update v4dydxob.py

--- v4orderbook/v4dydxob.py
+++ v4orderbook/v4dydxob.py
@@ -9,58 +9,67 @@ from random import randint
 from websocket import create_connection
 WSINDEXERURL = 'wss://indexer.v4testnet2.dydx.exchange/v4/ws'
 
-def checkaskfiles():
-        global zeroaskoffset
-        if os.path.isfile(ramdiskpath+'/'+market+'/asks/'+askprice) == True:
-                fp = open(ramdiskpath+'/'+market+'/asks/'+askprice)
-                line = fp.readline()
-                fname = line.strip('\n\r').split(sep)
-                fp.close()
-                faskoffset = fname[0]
-                fasksize = fname[1]
-        else:
-                faskoffset = 0
-        if ( os.path.isfile(ramdiskpath+'/'+market+'/asks/'+askprice) == False and int(askoffset) > zeroaskoffset ) or int(askoffset) > int(faskoffset):
-                if asksize == '0':
-                        if os.path.isfile(ramdiskpath+'/'+market+'/asks/'+askprice) == True:
-                                os.remove(ramdiskpath+'/'+market+'/asks/'+askprice)
-                                zeroaskoffset = int(askoffset)
-                else:
-                        fp = open(ramdiskpath+'/'+market+'/asks/'+askprice, "w")
-                        fp.write(str(askoffset)+' '+asksize+' '+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\n')
-                        fp.close()
-                logger.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' Updated '+ramdiskpath+'/'+market+'/asks/'+askprice+': '+str('('+asksize+')').ljust(10)+' '+str(askoffset))
+import pprint
+pp = pprint.PrettyPrinter(width = 41, compact = True)
 
-def checkbidfiles():
-        global zerobidoffset
-        if os.path.isfile(ramdiskpath+'/'+market+'/bids/'+bidprice) == True:
-                fp = open(ramdiskpath+'/'+market+'/bids/'+bidprice)
+def checkaskfiles(
+        framdiskpath,
+        fmarket,
+        faskprice,
+        fasksize,
+        faskoffset,
+):
+        global zeroaskoffset
+        if os.path.isfile(framdiskpath+'/'+fmarket+'/asks/'+faskprice) == True:
+                fp = open(framdiskpath+'/'+fmarket+'/asks/'+faskprice)
                 line = fp.readline()
                 fname = line.strip('\n\r').split(sep)
                 fp.close()
-                fbidoffset = fname[0]
-                fbidsize = fname[1]
+                ffaskoffset = fname[0]
+                ffasksize = fname[1]
         else:
-                fbidoffset = 0
-        if ( os.path.isfile(ramdiskpath+'/'+market+'/bids/'+bidprice) == False and int(bidoffset) > zerobidoffset ) or int(bidoffset) > int(fbidoffset):
-                if bidsize == '0':
-                        if os.path.isfile(ramdiskpath+'/'+market+'/bids/'+bidprice) == True:
-                                os.remove(ramdiskpath+'/'+market+'/bids/'+bidprice)
-                                zerobidoffset = int(bidoffset)
+                ffaskoffset = 0
+        if ( os.path.isfile(framdiskpath+'/'+fmarket+'/asks/'+faskprice) == False and int(faskoffset) > zeroaskoffset ) or int(faskoffset) > int(ffaskoffset):
+                if fasksize == '0':
+                        if os.path.isfile(framdiskpath+'/'+fmarket+'/asks/'+faskprice) == True:
+                                os.remove(framdiskpath+'/'+fmarket+'/asks/'+faskprice)
+                                zeroaskoffset = int(faskoffset)
                 else:
-                        fp = open(ramdiskpath+'/'+market+'/bids/'+bidprice, "w")
-                        fp.write(str(bidoffset)+' '+bidsize+' '+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\n')
+                        fp = open(framdiskpath+'/'+fmarket+'/asks/'+faskprice, "w")
+                        fp.write(str(faskoffset)+' '+fasksize+' '+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\n')
                         fp.close()
-                logger.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' Updated '+ramdiskpath+'/'+market+'/bids/'+bidprice+': '+str('('+bidsize+')').ljust(10)+' '+str(bidoffset))
+                logger.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' Updated '+framdiskpath+'/'+fmarket+'/asks/'+faskprice+': '+str('('+fasksize+')').ljust(10)+' '+str(faskoffset))
+
+def checkbidfiles(
+        framdiskpath,
+        fmarket,
+        fbidprice,
+        fbidsize,
+        fbidoffset,
+):
+        global zerobidoffset
+        if os.path.isfile(framdiskpath+'/'+fmarket+'/bids/'+fbidprice) == True:
+                fp = open(framdiskpath+'/'+fmarket+'/bids/'+fbidprice)
+                line = fp.readline()
+                fname = line.strip('\n\r').split(sep)
+                fp.close()
+                ffbidoffset = fname[0]
+                ffbidsize = fname[1]
+        else:
+                ffbidoffset = 0
+        if ( os.path.isfile(framdiskpath+'/'+fmarket+'/bids/'+fbidprice) == False and int(fbidoffset) > zerobidoffset ) or int(fbidoffset) > int(ffbidoffset):
+                if fbidsize == '0':
+                        if os.path.isfile(framdiskpath+'/'+fmarket+'/bids/'+fbidprice) == True:
+                                os.remove(framdiskpath+'/'+fmarket+'/bids/'+fbidprice)
+                                zerobidoffset = int(fbidoffset)
+                else:
+                        fp = open(framdiskpath+'/'+fmarket+'/bids/'+fbidprice, "w")
+                        fp.write(str(fbidoffset)+' '+fbidsize+' '+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'\n')
+                        fp.close()
+                logger.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' Updated '+framdiskpath+'/'+fmarket+'/bids/'+fbidprice+': '+str('('+fbidsize+')').ljust(10)+' '+str(fbidoffset))
 
 def openconnection():
         global ws
-        global askprice
-        global askoffset
-        global asksize
-        global bidprice
-        global bidoffset
-        global bidsize
         ws = create_connection(WSINDEXERURL)
         api_data = {
                 "type": "subscribe",
@@ -71,38 +80,25 @@ def openconnection():
         api_data = ws.recv()
         api_data = json.loads(api_data)
         print(api_data)
-        api_data = ws.recv()
-        api_data = json.loads(api_data)
-        askoffset = api_data['message_id']
-        bidoffset = api_data['message_id']
-        asks = api_data['contents']['asks']
-        bids = api_data['contents']['bids']
-        for askitem in asks:
-                askprice = askitem['price']
-                asksize = askitem['size']
-                checkaskfiles()
-                checkwidth('price', len(askprice))
-                checkwidth('size', len(asksize))
-        for biditem in bids:
-                bidprice = biditem['price']
-                bidsize = biditem['size']
-                checkbidfiles()
-                checkwidth('price', len(bidprice))
-                checkwidth('size', len(bidsize))
 
-def checkwidth(elementname, elementsize):
+def checkwidth(
+        framdiskpath,
+        fmarket,
+        felementname,
+        felementsize
+):
         global maxwidthprice
         global maxwidthsize
-        if elementname == 'price' and elementsize > maxwidthprice:
-                fp = open(ramdiskpath+'/'+market+'/maxwidth'+elementname, "w")
-                fp.write(str(elementsize)+'\n')
+        if felementname == 'price' and felementsize > maxwidthprice:
+                fp = open(framdiskpath+'/'+fmarket+'/maxwidth'+felementname, "w")
+                fp.write(str(felementsize)+'\n')
                 fp.close()
-                maxwidthprice = elementsize
-        elif elementname == 'size' and elementsize > maxwidthsize:
-                fp = open(ramdiskpath+'/'+market+'/maxwidth'+elementname, "w")
-                fp.write(str(elementsize)+'\n')
+                maxwidthprice = felementsize
+        elif felementname == 'size' and felementsize > maxwidthsize:
+                fp = open(framdiskpath+'/'+fmarket+'/maxwidth'+felementname, "w")
+                fp.write(str(felementsize)+'\n')
                 fp.close()
-                maxwidthsize = elementsize
+                maxwidthsize = felementsize
 
 print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' v4dydxob.py')
 logger = logging.getLogger("Rotating Log")
@@ -146,28 +142,101 @@ while True:
                 api_data = json.loads(api_data)
                 askoffset = api_data['message_id']
                 bidoffset = api_data['message_id']
-                if 'asks' in api_data['contents'].keys():
-                        asks = api_data['contents']['asks']
-                else:
+                if isinstance(api_data['contents'], dict):
+                        if 'asks' in api_data['contents'].keys():
+                                asks = api_data['contents']['asks']
+                        else:
+                                asks = []
+                        if 'bids' in api_data['contents'].keys():
+                                bids = api_data['contents']['bids']
+                        else:
+                                bids = []
+                elif isinstance(api_data['contents'], list):
                         asks = []
-                if 'bids' in api_data['contents'].keys():
-                        bids = api_data['contents']['bids']
-                else:
                         bids = []
+                        for item in api_data['contents']:
+                                if 'asks' in item.keys():
+                                        for bitem in item['asks']:
+                                                if isinstance(bitem, dict):
+                                                        askprice = bitem['price']
+                                                        asksize = bitem['size']
+                                                elif isinstance(bitem, list):
+                                                        askprice = bitem[0]
+                                                        asksize = bitem[1]
+                                                askitem = {
+                                                        "price": askprice,
+                                                        "size": asksize
+                                                }
+                                                asks.append(askitem)
+                                if 'bids' in item.keys():
+                                        for bitem in item['bids']:
+                                                if isinstance(bitem, dict):
+                                                        bidprice = bitem['price']
+                                                        bidsize = bitem['size']
+                                                elif isinstance(bitem, list):
+                                                        bidprice = bitem[0]
+                                                        bidsize = bitem[1]
+                                                biditem = {
+                                                        "price": bidprice,
+                                                        "size": bidsize
+                                                }
+                                                bids.append(biditem)
+
+
                 if asks != []:
                         for askitem in asks:
-                                askprice = askitem[0]
-                                asksize = askitem[1]
-                                checkaskfiles()
-                                checkwidth('price', len(askprice))
-                                checkwidth('size', len(asksize))
+                                if isinstance(askitem, dict):
+                                        askprice = askitem['price']
+                                        asksize = askitem['size']
+                                elif isinstance(askitem, list):
+                                        askprice = askitem[0]
+                                        asksize = askitem[1]
+                                checkaskfiles(
+                                        framdiskpath = ramdiskpath,
+                                        fmarket = market,
+                                        faskprice = askprice,
+                                        fasksize = asksize,
+                                        faskoffset = askoffset,
+                                )
+                        checkwidth(
+                                framdiskpath = ramdiskpath,
+                                fmarket = market,
+                                felementname = 'price',
+                                felementsize = len(askprice)
+                        )
+                        checkwidth(
+                                framdiskpath = ramdiskpath,
+                                fmarket = market,
+                                felementname = 'size',
+                                felementsize = len(asksize)
+                        )
                 if bids != []:
                         for biditem in bids:
-                                bidprice = biditem[0]
-                                bidsize = biditem[1]
-                                checkbidfiles()
-                                checkwidth('price', len(bidprice))
-                                checkwidth('size', len(bidsize))
+                                if isinstance(biditem, dict):
+                                        bidprice = biditem['price']
+                                        bidsize = biditem['size']
+                                elif isinstance(biditem, list):
+                                        bidprice = biditem[0]
+                                        bidsize = biditem[1]
+                                checkbidfiles(
+                                        framdiskpath = ramdiskpath,
+                                        fmarket = market,
+                                        fbidprice = bidprice,
+                                        fbidsize = bidsize,
+                                        fbidoffset = bidoffset,
+                                )
+                        checkwidth(
+                                framdiskpath = ramdiskpath,
+                                fmarket = market,
+                                felementname = 'price',
+                                felementsize = len(bidprice)
+                        )
+                        checkwidth(
+                                framdiskpath = ramdiskpath,
+                                fmarket = market,
+                                felementname = 'size',
+                                felementsize = len(bidsize)
+                        )
         except KeyboardInterrupt:
                 ws.close()
                 sys.exit(0)
