5cbb36832653e0ea39d5d3ff4b3b2422b00223b7
chiwalfrm <chiuljnk@hotmail.com>
2023-10-23 11:23:43 -0500

    Create v4convert_bip39_seed_to_dydxv4seed.js

---
+++ v4convert_bip39_seed_to_dydxv4seed.js
@@ -0,0 +1,49 @@
+const { deriveHDKeyFromEthereumSignature } = require('@dydxprotocol/v4-client-js/build/src/lib/onboarding.js');
+const { BECH32_PREFIX } = require('@dydxprotocol/v4-client-js/build/src');
+const LocalWallet = require('@dydxprotocol/v4-client-js/build/src/clients/modules/local-wallet').default;
+const { ethers } = require('ethers');
+const fs = require('fs');
+
+// ########################## YOU FILL THIS OUT #################
+const mnemonic = '<FILL_THIS_OUT>'
+// ##############################################################
+
+const wallet = ethers.Wallet.fromPhrase(mnemonic);
+
+const toSign = {
+  "domain": {
+    "name": "dYdX V4",
+    "chainId": 5
+  },
+  "primaryType": "dYdX",
+  "types": {
+    "EIP712Domain": [
+      {
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "name": "chainId",
+        "type": "uint256"
+      }
+    ],
+    "dYdX": [
+      {
+        "name": "action",
+        "type": "string"
+      }
+    ]
+  },
+  "message": {
+    "action": "dYdX Chain Onboarding"
+  }
+};
+
+(async () => {
+  const signature = await wallet.signTypedData(toSign.domain, { dYdX: toSign.types.dYdX }, toSign.message);
+  console.log('Signed Data:', signature);
+  const k = deriveHDKeyFromEthereumSignature(signature);
+  console.log('Mnemonic:', k.mnemonic);
+  const lw = await LocalWallet.fromMnemonic(k.mnemonic, BECH32_PREFIX);
+  console.log('Address:', lw.address)
+})();
